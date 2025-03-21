100673171a2b1521cd460cbac0368eec4820605f
Isaac <ederaldohuss@gmail.com>
2025-03-21 12:17:04 -0300

    commit 3

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ function App() {
           <div className="container">
             <div className="divlogo">
               <div className="img">
-                <img className="logo" src="/images/icons8-cd-100.png" alt="Logo" />
+                <img className="logo" src="https://images.vexels.com/media/users/3/153242/isolated/preview/6076a2083aacc2b3affbc0cc7a35c0d7-icone-plano-do-disco-compacto-multimidia.png" alt="Logo" />
               </div>
               <div className="h1">
                 <h1>MUZAAC</h1>
